423a804903c2fc7a13876edd6ac43aadd99a40de
NikhilNB <nbarapatre06@gmail.com>
2024-01-26 12:30:17 +0530

    Add project files.

---
+++ GitHubProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            addition();
+
+        }
+
+        public static void addition()
+        {
+            int x = 10, y = 20, z;
+            z = x + y;
+            Console.WriteLine("Addition is :"+z);
+        }
+    }
+}
